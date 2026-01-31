270db9bccaf732ffa32dea6cc71b532fde5ac03b
Dr. Takeyuki UEDA <ueda@latelierdueda.com>
2026-01-31 10:33:51 +0900

    test2

---
+++ test2/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+    <Navbar
+      titleStr="Demo App" :links="links" qrExp=""
+    />
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import Navbar from '../../src/components/Navbar.vue'
+const links = [
+      { icon: '', text: 'Home', route: '/'},
+    ]
+  //
+</script>
